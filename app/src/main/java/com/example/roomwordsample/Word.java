1d42685c92bc063bebea365597d26c3a84032650
tonyBred <galihrizkisetiadi18@gmail.com>
2021-04-21 14:48:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/roomwordsample/Word.java
@@ -0,0 +1,17 @@
+package com.example.roomwordsample;
+
+import androidx.annotation.NonNull;
+import androidx.room.*;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public Word(@NonNull String word) {this.mWord = word;}
+
+    public String getWord(){return this.mWord;}
+}
---
+++ app/src/main/java/com/example/roomwordsample/WordDao.java
@@ -0,0 +1,21 @@
+package com.example.roomwordsample;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.*;
+
+import java.util.List;
+
+@Dao
+public interface WordDao {
+
+    // allowing the insert of the same word multiple times by passing a
+    // conflict resolution strategy
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAlphabetizedWords();
+}
